59e3557a45bd5347333cfa0cca1a0b5d036e6d8f
Mohamed Mahfouz <60847315+mohamed-mahfouz@users.noreply.github.com>
2022-03-02 02:51:34 +0200

    update project to meet all requirements

--- js/app.js
+++ js/app.js
@@ -7,55 +7,70 @@ const navBar = document.getElementById('navbar__list');
 //Create links and add them dynamically to navbar.
 for(const section of sections)
 {
-const sectionId = section.getAttribute('id');
-const sectionName = section.getAttribute('data-nav');
-const link = document.createElement('a');
-link.textContent = sectionName;
-link.setAttribute('href',`#${sectionId}`);
-link.className = 'menu__link';
-const listItem = document.createElement('li');
-listItem.appendChild(link);
-navBar.appendChild(listItem);
+  const sectionId = section.getAttribute('id');
+  const sectionName = section.getAttribute('data-nav');
+  const link = document.createElement('a');
+  link.textContent = sectionName;
+  link.setAttribute('href',`#${sectionId}`);
+  link.className = 'menu__link';
+  const listItem = document.createElement('li');
+  listItem.appendChild(link);
+  navBar.appendChild(listItem);
 }
 
 
-// Add class active to section that user scrolled to.
-window.onscroll = () => {
-  const scrollPosition = document.body.scrollTop;
-  
-    sections.forEach((section) => 
-    {
-    if (scrollPosition+10 >= section.offsetTop && 
-        scrollPosition < section.offsetTop + section.offsetHeight) 
-    {
-        const currentId = section.attributes.id.value;
-        removeAllActiveClasses();
-        addActiveClass(currentId);
-    }
-  });
-};
-
-//Remove active class if it is not viewed in my current viewport.
-function removeAllActiveClasses(){
+//Remove highlight class from navbar links.
+function removeHightlightClass(){
     const navListItem = document.querySelectorAll("nav ul li");
-    navListItem.forEach(item=>{
-      item.classList.remove('active');
+      navListItem.forEach(item=>{
+      item.classList.remove('highlight');
     });
 }
 
-//add active class to list item in nav bar.
-function addActiveClass(id){
-  const selector = `nav a[href="#${id}"]`;
-  document.querySelector(selector).parentElement.classList.add('active');
+//Add highlight class to list item in navbar for highligting.
+function addHighlightClass(id){
+  const selectorForNavLinks = `nav a[href="#${id}"]`;
+  document.querySelector(selectorForNavLinks).parentElement.classList.add("highlight");
 }
+
+//Add active class to current section that in viewport.
+function AddActiveClass(id){
+  document.querySelector(`#${id}`).classList.add("active");
+}
+
+//Remove class atribute that had active class at section element.
+function removeActiveClass()
+{ 
+    sections.forEach(function(section){
+      section.classList.remove("active");
+    });
+     
+}
+
+window.addEventListener("scroll", ()=>{
  
+  sections.forEach(function(section)
+  {    
+    if(section.getBoundingClientRect().top < window.innerHeight)
+    {
+      
+      //get current section id that apperas in browser viewport.
+      const currentSectionId= section.attributes.id.value;
+      removeActiveClass();
+      removeHightlightClass();
+      addHighlightClass(currentSectionId);
+      AddActiveClass(currentSectionId);
+    }
+           
+  });
+});
+
 
 //Get links that are in my navigation menu using querySelectorAll.
 const links =  document.querySelectorAll('a[href^="#"]');
 
 //Register event handler to all links.
 for (const link of links) {
-  console.log(link);
   link.addEventListener("click", clickHandler);
 }
 
@@ -72,6 +87,10 @@ function clickHandler(event) {
     behavior: "smooth"
   });
 }
+
+
+
+
 
 
 
